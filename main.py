37e368976367ff1985a4c2c867625c6c171c6fb0
WeReal <94121668+wereal@users.noreply.github.com>
2022-12-22 20:57:47 +0200

    Calendar exp
    simple calendar fanction exp

---
+++ main.py
@@ -0,0 +1,22 @@
+import calendar 
+import datetime
+import time 
+print()
+print(calendar.weekheader(3)) 
+print()
+print(calendar.firstweekday())
+print()
+print(calendar.month(2023, 1))
+print()
+
+#print(calendar.monthcalendar(2023, 1))
+ 
+#
+#day_of_the_week = calendar.weekday(2022, 3, 8)
+#print(day_of_the_week)
+
+#is_leap = calendar.isleap(2023)
+#print(is_leap)
+
+
+
